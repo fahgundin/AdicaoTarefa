0e704efc14415e174ffd928f63abfaa593356b5f
Arthur Fagundes <134743564+fahgundin@users.noreply.github.com>
2024-02-07 14:13:11 -0400

    Add files via upload

---
+++ Adicao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+import javax.swing.*;
+
+public class Adicao {
+
+   
+    public static void main(String[] args) {
+        
+     
+ 
+        JOptionPane.showMessageDialog(null,(Integer.parseInt(JOptionPane.showInputDialog("Enter First Integer"))+ Integer.parseInt(JOptionPane.showInputDialog("Enter Second Integer"))));
+        
+    }
+    
+}
